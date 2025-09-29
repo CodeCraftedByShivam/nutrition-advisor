f8252b7b01380d449e900134119bd914e5a3ad0e
CodeCraftedByShivam <shivamyadav154155@gmail.com>
2025-09-29 21:37:43 +0530

    Prepare for Render deployment - add requirements and start script

--- api/index.py
+++ api/index.py
@@ -637,4 +637,4 @@ def save_profile(current_user):
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
---
+++ start.sh
@@ -0,0 +1,2 @@
+﻿#!/bin/bash
+gunicorn --bind 0.0.0.0: api.index:app
